99295ea5af1d39ad1d3e13eea421601bacbe75bf
Maria Wright <maria@mariawrightstudios.com>
2021-08-17 00:18:10 -0400

    add styled wrapper comp & edit App.js with example

--- src/Components/Wrapper.js
+++ src/Components/Wrapper.js
@@ -1,16 +1,33 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.section`
   width: 100%;
-  max-width: 1280px;
-  background color: ${(props) => {
-    if (props.neutral) return '#e5e5e5';
-    else if (props.dark) return '#000';
-    else return '#fff';
-  }};
-  color: ${(props) => { props.dark ? '#fff' : '#000' }};
-  margin: 100px 80px;
+  background-color: #fff;
+  color: #000;
+  padding: 100px 80px;
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  box-sizing: border-box;
+  ${({ dark }) =>
+    dark &&
+    css`
+      background-color: #000;
+      color: #fff;
+  `}
+  ${({ neutral }) =>
+    neutral &&
+    css`
+      background-color: #e5e5e5;
+      color: #000;
+  `}
+
+  @media (min-width: 321px) and (max-width: 1024px) {
+    padding: 100px 40px;
+  }
+
+  @media (max-width: 320px) {
+    padding: 60px 16px;
+  }
 `;
