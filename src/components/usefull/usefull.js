cc6071367da59f255725caf181f550b2bcf8034d
Maria Wright <maria@mariawrightstudios.com>
2021-08-22 22:27:53 -0400

    refactor usefull section

--- src/components/usefull/usefull.js
+++ src/components/usefull/usefull.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Container, CardWrapper, UsefullCard, UsefullTitle, UsefullIntro } from './usefull.styles';
 import { Wrapper } from '../Wrapper';
 
@@ -8,33 +9,33 @@ import image3 from '../../images/pr-boost.png';
 const Usefull = () => {
     return (
         <Wrapper dark>
-        <Container>
+            <Container>
 
-            <UsefullTitle>Why is it useful for your company?</UsefullTitle>
-            <UsefullIntro>By handing over assignments to students for work, you free experienced employees from routine tasks, as well as increase brand awareness.</UsefullIntro>
+                <UsefullTitle>Why is it useful for your company?</UsefullTitle>
+                <UsefullIntro>By handing over assignments to students for work, you free experienced employees from routine tasks, as well as increase brand awareness.</UsefullIntro>
 
-            <CardWrapper>
-                <UsefullCard
-                    usefullImage={image1}
-                    usefullTitle={'Resource saving'}
-                    usefullText={'Delegate tasks for free and get results that can be used in your future work.'}
-                >
-                </UsefullCard>
-                <UsefullCard
-                    usefullImage={image2}
-                    usefullTitle={'Publicize'}
-                    usefullText={'We take pride in the real-world value of the tasks that our students perform and we share them in all available channels.'}
-                >
-                </UsefullCard>
-                <UsefullCard
-                    usefullImage={image3}
-                    usefullTitle={'PR boost'}
-                    usefullText={'This will increase the visibility of the company among job seekers and attract those who share the values of your company.'}
-                >
-                </UsefullCard>
-            </CardWrapper>
+                <CardWrapper>
+                    <UsefullCard
+                        usefullImage={image1}
+                        usefullTitle={'Resource saving'}
+                        usefullText={'Delegate tasks for free and get results that can be used in your future work.'}
+                    >
+                    </UsefullCard>
+                    <UsefullCard
+                        usefullImage={image2}
+                        usefullTitle={'Publicize'}
+                        usefullText={'We take pride in the real-world value of the tasks that our students perform and we share them in all available channels.'}
+                    >
+                    </UsefullCard>
+                    <UsefullCard
+                        usefullImage={image3}
+                        usefullTitle={'PR boost'}
+                        usefullText={'This will increase the visibility of the company among job seekers and attract those who share the values of your company.'}
+                    >
+                    </UsefullCard>
+                </CardWrapper>
 
-        </Container>
+            </Container>
         </Wrapper>
     )
 }
