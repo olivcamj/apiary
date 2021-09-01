fc5697fc60abe9efbc15052e29db638db049fc5a
unknown <franjseco@gmail.com>
2021-09-01 10:01:43 +0200

    removing list marker

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -5,13 +5,17 @@ const List = styled.ul`
   padding: 0;
 `;
 
+const Li = styled.li`
+  list-style-type: none;
+`;
+
 const ProjectList = ({ projectContent, indexShow }) => {
   return (
     <List>
       {projectContent.filter((element, i) => i < indexShow).map((project, index) => (
-        <li key={index}>
+        <Li key={index}>
           <ProjectCardGroup key={index} element={project}/>
-        </li>
+        </Li>
       ))}
     </List>
   )
