2fdf40a921ae9d8acfb36c506980be3e6717ea0c
Maria Wright <maria@mariawrightstudios.com>
2021-09-01 11:22:46 -0400

    add loop for CardBlock and code css for numbers

--- src/components/WhyChoose.js
+++ src/components/WhyChoose.js
@@ -6,31 +6,23 @@ import image1 from "../images/quality.png";
 import image2 from "../images/professionalism.png";
 import image3 from "../images/delegate.png";
 
-const whyChooseCards = (
-  <>
-    <IconBoxCard
-      iconBoxImage={image1}
-      iconBoxTitle={"Quality"}
-      iconBoxText={
-        "The Yandex search engine is the fourth ranked search engine in the world in terms of number of requests processed."
-      }
-    />
-    <IconBoxCard
-      iconBoxImage={image2}
-      iconBoxTitle={"Professionalism"}
-      iconBoxText={
-        "Practicum by Yandex is one of the most popular online education services in Russia, allowing students to get a new profession or master a new skill."
-      }
-    />
-    <IconBoxCard
-      iconBoxImage={image3}
-      iconBoxTitle={"Experience"}
-      iconBoxText={
-        "Our students are already working at Accenture, Tesla, Invitae. Our specialty is practical training for the real world."
-      }
-    />
-  </>
-);
+const whyChooseCards = [
+  {
+    iconBoxImage: image1,
+    iconBoxTitle: "Quality",
+    iconBoxText: "The Yandex search engine is the fourth ranked search engine in the world in terms of number of requests processed."
+  },
+  {
+    iconBoxImage: image2,
+    iconBoxTitle: "Professionalism",
+    iconBoxText: "Practicum by Yandex is one of the most popular online education services in Russia, allowing students to get a new profession or master a new skill."
+  },
+  {
+    iconBoxImage: image3,
+    iconBoxTitle: "Experience",
+    iconBoxText: "Our students are already working at Accenture, Tesla, Invitae. Our specialty is practical training for the real world."
+  }
+]
 
 const WhyChoose = () => {
   return (
@@ -39,7 +31,16 @@ const WhyChoose = () => {
         color="dark"
         title="Why choose us?"
         intro="We have built the largest IT company in Russia. We know how to develop cool employees, and we work hard to make our clients' lives better and happier."
-        cards={whyChooseCards}
+        cards={whyChooseCards.map((card, i) => {
+          return (
+            <IconBoxCard
+              key={i}
+              iconBoxImage={card.iconBoxImage}
+              iconBoxTitle={card.iconBoxTitle}
+              iconBoxText={card.iconBoxText}
+            />
+          )
+        })}
       />
     </Wrapper>
   );
